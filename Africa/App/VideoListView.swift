297f2e94fc36047b8483399d6a692d522f0c8037
rifqimuhammadaziz <rifqimuhammadaziz@gmail.com>
2023-04-03 22:39:06 +0700

    setup code structure & add tab view

---
+++ Africa/App/VideoListView.swift
@@ -0,0 +1,20 @@
+//
+//  VideoListView.swift
+//  Africa
+//
+//  Created by Rifqi Muhammad Aziz on 03/04/23.
+//
+
+import SwiftUI
+
+struct VideoListView: View {
+    var body: some View {
+        Text("Videos")
+    }
+}
+
+struct VideoListView_Previews: PreviewProvider {
+    static var previews: some View {
+        VideoListView()
+    }
+}
