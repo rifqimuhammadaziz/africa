be0f4f873e00b7ba98deb74ecde4af33bd305134
rifqimuhammadaziz <rifqimuhammadaziz@gmail.com>
2023-04-03 22:03:54 +0700

    Initial Commit

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Rifqi Muhammad Aziz on 03/04/23.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
