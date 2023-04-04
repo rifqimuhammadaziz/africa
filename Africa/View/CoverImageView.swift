b1e23492d058079b730359fbae13a4efc3ee7aff
rifqimuhammadaziz <rifqimuhammadaziz@gmail.com>
2023-04-04 19:51:08 +0700

    create cover image view

---
+++ Africa/View/CoverImageView.swift
@@ -0,0 +1,35 @@
+//
+//  CoverImageView.swift
+//  Africa
+//
+//  Created by Rifqi Muhammad Aziz on 04/04/23.
+//
+
+import SwiftUI
+
+struct CoverImageView: View {
+    // MARK: - PROPERTIES
+    
+    // MARK: - BODY
+    
+    var body: some View {
+        TabView {
+            ForEach(/*@START_MENU_TOKEN@*/0 ..< 5/*@END_MENU_TOKEN@*/) { item in
+                Image("cover-lion")
+                    .resizable()
+                .scaledToFit()
+            } //: LOOP
+        } //: TAB
+        .tabViewStyle(PageTabViewStyle())
+        
+    }
+}
+
+// MARK: - PREVIEW
+
+struct CoverImageView_Previews: PreviewProvider {
+    static var previews: some View {
+        CoverImageView()
+            .previewLayout(.fixed(width: 400, height: 300))
+    }
+}
