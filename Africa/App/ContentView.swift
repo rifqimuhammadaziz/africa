88b355638c15e71d4536197421079218936e484c
rifqimuhammadaziz <rifqimuhammadaziz@gmail.com>
2023-04-06 21:36:07 +0700

    create carousel with json data

--- Africa/App/ContentView.swift
+++ Africa/App/ContentView.swift
@@ -9,8 +9,18 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Content")
-            .padding()
+        // MARK: - PROPERTIES
+        
+        // MARK: - BODY
+        NavigationView {
+            List {
+                CoverImageView()
+                    .frame(height: 300)
+                    .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
+            } //: LIST
+            .navigationTitle("Africa")
+            .navigationBarTitleDisplayMode(.large)
+        } //: NAVIGATION
     }
 }
 
---
+++ Africa/Model/CoverImageModel.swift
@@ -0,0 +1,13 @@
+//
+//  CoverImageModel.swift
+//  Africa
+//
+//  Created by Rifqi Muhammad Aziz on 04/04/23.
+//
+
+import SwiftUI
+
+struct CoverImage: Codable, Identifiable {
+    let id: Int
+    let name: String
+}
