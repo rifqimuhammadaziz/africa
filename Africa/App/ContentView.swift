77308ad25db1a21ad65b2f0129ad618a132cd256
rifqimuhammadaziz <rifqimuhammadaziz@gmail.com>
2023-04-07 22:26:38 +0700

    create dynamic list view using swift generics and static json data

--- Africa/App/ContentView.swift
+++ Africa/App/ContentView.swift
@@ -11,12 +11,18 @@ struct ContentView: View {
     var body: some View {
         // MARK: - PROPERTIES
         
+        let animals: [Animal] = Bundle.main.decode("animals.json")
+        
         // MARK: - BODY
         NavigationView {
             List {
                 CoverImageView()
                     .frame(height: 300)
                     .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
+                
+                ForEach(animals) { animal in
+                    AnimalListItemView(animal: animal)
+                }
             } //: LIST
             .navigationTitle("Africa")
             .navigationBarTitleDisplayMode(.large)
