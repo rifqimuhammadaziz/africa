2d67a41038c03ed033c9623e3f5305948eed6d53
rifqimuhammadaziz <rifqimuhammadaziz@gmail.com>
2023-04-09 15:34:44 +0700

    create video player

---
+++ Africa/View/VideoPlayerView.swift
@@ -0,0 +1,48 @@
+//
+//  VideoPlayerView.swift
+//  Africa
+//
+//  Created by Rifqi Muhammad Aziz on 09/04/23.
+//
+
+import SwiftUI
+import AVKit
+
+struct VideoPlayerView: View {
+    // MARK: - PROPERTIES
+    
+    var videoSelected: String
+    var videoTitle: String
+    
+    // MARK: - BODY
+    
+    var body: some View {
+        VStack {
+            VideoPlayer(player: playVideo(fileName: videoSelected, fileFormat: "mp4")) {
+//                Text(videoTitle)
+            } //: VIDEOPLAYER
+            .overlay(
+                Image("logo")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 32, height: 32)
+                    .padding(.top, 6)
+                    .padding(.horizontal, 8),
+                alignment: .topLeading
+            )
+        } //: VSTACK
+        .tint(.accentColor)
+        .navigationTitle(videoTitle)
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+// MARK: - PREVIEW
+
+struct VideoPlayerView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            VideoPlayerView(videoSelected: "lion", videoTitle: "Lion")
+        }
+    }
+}
